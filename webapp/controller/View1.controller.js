1f5117a5132db623863a910203b18b3d822c9caa
Antonio Cuciniello <antonio.cuciniello@accenture.com>
2019-12-16 10:53:52 +0000

    Ho inserito il commento "prova" sul file controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
+	//prova
 	return Controller.extend("barilla.barilla.controller.View1", {
 		onInit: function () {
 
