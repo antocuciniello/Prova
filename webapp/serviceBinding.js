261f9d4a716c7668214b1ddf18608c5fb65ad698
Antonio Cuciniello <antonio.cuciniello@accenture.com>
2019-12-16 10:12:32 +0000

    prova prima commit

---
+++ webapp/serviceBinding.js
@@ -0,0 +1,5 @@
+function initModel() {
+	var sUrl = "/anto/barilla_service.xsodata/";
+	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
+	sap.ui.getCore().setModel(oModel);
+}
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"barilla/barilla/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
